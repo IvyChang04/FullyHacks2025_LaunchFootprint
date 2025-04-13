e09c08c9a677579685f0811b3500031a4428408a
bartsuper <bartsuper@icloud.com>
2025-04-13 11:13:38 -0700

    added flask-cors

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse, abort
 import csv
 from location_lon_lat import locations
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class Test(Resource):
